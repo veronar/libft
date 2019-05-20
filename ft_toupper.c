aa689294a0060a8418309d77bbc0e123e723c249
Verona SINGH <vesingh@c4r15s1.wethinkcode.co.za>
2019-05-20 12:12:58 +0200

    initial ft_toupper.c

---
+++ ft_toupper.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 10:53:09 by vesingh           #+#    #+#             */
+/*   Updated: 2019/05/20 12:12:32 by vesingh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		c = c - 32;
+		return (c);
+	}
+	else
+		return (c);
+}
+
+
+/*
+#include <ctype.h>
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+	if (ac != 2)
+		return 0;
+	else if (av[1][1] != '\0')
+		return 0;
+	else 
+	{
+		a = ft_toupper(av[1][0]);
+		printf("ft_toupper: %d", a);
+		printf("\n");
+		b = toupper(av[1][0]);
+		printf("toupper: %d", b);
+		printf("\n");
+	}
+	return 0;
+}*/
