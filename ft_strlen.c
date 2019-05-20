6afb1458bee5a84af8fabd69f36a5b7d35fcca00
Verona SINGH <vesingh@c4r15s1.wethinkcode.co.za>
2019-05-20 10:43:34 +0200

    fixed strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,16 +6,38 @@
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 06:48:27 by vesingh           #+#    #+#             */
-/*   Updated: 2019/05/20 08:53:27 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/05/20 10:42:43 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str)
 {
-	int i;
+	size_t i;
 
 	i = 0;
 	while (str[i] != '\0')
 		i++;
 	return (i);
 }
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	size_t a;
+
+	a = 0;
+	if (ac != 2)
+		return 0;
+	else 
+	{
+		a = ft_strlen(av[1]);
+		printf("Your string length is: %zu", a);
+		printf("\n");
+	}
+	return 0;
+}
+*/
