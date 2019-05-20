b7066dacb30c8852f8d7e63a1ab33d8335053900
Verona SINGH <vesingh@c4r15s1.wethinkcode.co.za>
2019-05-20 12:16:52 +0200

    initial ft_tolower.c

---
+++ ft_tolower.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 10:53:09 by vesingh           #+#    #+#             */
+/*   Updated: 2019/05/20 12:16:16 by vesingh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		c = c + 32;
+		return (c);
+	}
+	else
+		return (c);
+}
+
+/*
+#include <ctype.h>
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+	if (ac != 2)
+		return 0;
+	else if (av[1][1] != '\0')
+		return 0;
+	else 
+	{
+		a = ft_tolower(av[1][0]);
+		printf("ft_tolower: %d", a);
+		printf("\n");
+		b = tolower(av[1][0]);
+		printf("tolower: %d", b);
+		printf("\n");
+	}
+	return 0;
+}*/
