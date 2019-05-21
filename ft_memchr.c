69059c8883a6678ed63f85446688829db4595894
Verona SINGH <vesingh@c4r18s3.wethinkcode.co.za>
2019-05-21 14:18:27 +0200

    initial memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,37 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 10:01:43 by vesingh           #+#    #+#             */
-/*   Updated: 2019/05/21 13:39:54 by vesingh          ###   ########.fr       */
+/*   Created: 2019/05/21 13:41:41 by vesingh           #+#    #+#             */
+/*   Updated: 2019/05/21 14:13:48 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memchr(const void *str, int c, size_t n)
 {
-	size_t 		i;
-	const char 	*csrc;
-	char 		*cdest;
-	char 		temp[len];
+	size_t			i;
+	unsigned char	chr;
+	unsigned char	*cstr;
 
 	i = 0;
-	csrc = (const char*)src;
-	cdest = (char*)dst;
-	while(i < len)
+	chr = (unsigned char)c;
+	cstr = (unsigned char*)str;
+ 	while(i < n)
 	{
-		temp[i] = csrc[i];
+		if(cstr[i] == chr)
+			return(cstr + i);
 		i++;
 	}
-	i = 0;
-	while(i < len)
-	{
-		cdest[i] = temp[i];
-		i++;
-	}
-	return(dst);
+	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:03:10 by vesingh           #+#    #+#             */
-/*   Updated: 2019/05/21 12:53:13 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/05/21 13:57:30 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ char 	*ft_strcpy(char *dst, const char *src);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *str, int c, size_t n);
 
 
 #endif
