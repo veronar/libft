c64af1d984dec1dc1acbccddc73d4c5df8f8ae37
Verona SINGH <vesingh@c4r15s2.wethinkcode.co.za>
2019-05-23 11:44:12 +0200

    admin sorting

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,13 +6,27 @@
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:21:20 by vesingh           #+#    #+#             */
-/*   Updated: 2019/05/22 07:27:08 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/05/23 11:39:15 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcmp(const void *s1, const void *s2)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	return(ft_memcmp(s1, s2, (ft_strlen(s1)+1)));
+	size_t				i;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+
+	i = 0;
+	str1 = (const unsigned char*)s1;
+	str2 = (const unsigned char*)s2;
+	while(i < n)
+	{
+		if(str1[i] == str2[i])
+			i++;
+		else
+			return(*str1 - *str2);
+	}
+	return (0);
 }
