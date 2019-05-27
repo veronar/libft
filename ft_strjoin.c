2e1377dfc4fdbbef6b3e091a21ae61ee7e6a0421
Verona SINGH <vesingh@c4r15s2.wethinkcode.co.za>
2019-05-27 13:31:33 +0200

    Part 2 progress

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 11:35:19 by vesingh           #+#    #+#             */
-/*   Updated: 2019/05/27 12:08:51 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/05/27 13:30:57 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,13 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*mem;
-	int 	i;
-	int		j;
 
-	i = 0;
-	j = 0;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	mem = (char*)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (mem == NULL)	
-		return (NULL);
-	while (s1[i] != '\0')
-	{
-		mem[i] = s1[i];
-		i++;
-	}
-	i--;
-	while (s2[j] != '\0')
-	{
-		mem[i] = s2[j];
-		i++;
-		j++;
-	}
-	mem[i] = '\0';
+		return (NULL);	
+	ft_strcpy(mem, s1);
+	ft_strcat(mem, s2);
 	return (mem);
 }
