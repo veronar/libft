1a77f8caff1e2d2a8a53c1599783bae3372c9b6a
Verona SINGH <vesingh@c4r15s2.wethinkcode.co.za>
2019-05-29 14:11:46 +0200

    itoa progress

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,35 +6,52 @@
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 11:41:12 by vesingh           #+#    #+#             */
-/*   Updated: 2019/05/29 12:43:13 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/05/29 13:18:07 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_itoa_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		len++;
+	while (n)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
+	int		len;
 	char	*result;
-	int		i;
-	
-	i = 0;
-	if (!(result = (char*)ft_memalloc(sizeof(char) * 13)))
+	int		neg;
+
+	len = ft_itoa_len(n);
+	neg = 0;
+	if (!(result = (char*)ft_memalloc(sizeof(char) * (len + 1))))
 		return (NULL);
+	result[len] = '\0';
 	if (n == -2147483648)
-		return (result = "-2147483648");
-	if (n > 0)
+	   return (result = "-2147483648");	
+	if (n < 0)
 	{
-		result[i] = '-';
-		n = n * -1;
-		i++;
+		result[0] = '-';
+		neg = 1;
 	}
-	while (n > 9)
+	while (len > neg)
 	{
-		result[i] = n / 10;
-		i++;
+		len--;
+		result[len] = (n % 10) + 48;
 		n = n / 10;
 	}
-	result[i] = n % 10;
-	result[i + 1] = '\0';
 	return (result);
 }
