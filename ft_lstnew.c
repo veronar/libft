95e8c5ea80b097194120ce1bc6d95e74ca550213
Verona SINGH <vesingh@c4r14s3.wethinkcode.co.za>
2019-05-30 15:45:52 +0200

    1st bonus completed

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -5,13 +5,47 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/30 09:46:41 by vesingh           #+#    #+#             */
-/*   Updated: 2019/05/30 12:27:21 by vesingh          ###   ########.fr       */
+/*   Created: 2019/05/30 15:05:46 by vesingh           #+#    #+#             */
+/*   Updated: 2019/05/30 15:43:37 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*t_list	*ft_lstnew(void const *content, size_t content_size)
-{
-	
+#include "libft.h"
 
+t_list	*ft_lstnew(void const *c, size_t n)
+{
+	t_list	*new;
+
+	if (!(new = ft_memalloc(sizeof(t_list))))
+		return (NULL);
+	if (c == NULL)
+	{
+		new->content = NULL;
+		new->content_size = 0;
+	}
+	else
+	{
+		if (!(new->content = ft_memalloc(sizeof(n))))
+		{
+			free(new);
+			return (NULL);
+		}
+		new->content = ft_memcpy(new->content, c, n);
+		new->content_size = n;
+	}
+	new->next = (NULL);
+	return (new);
+}
+/*
+int	main()
+{
+	size_t i = 1;
+	char c = 'a';
+
+	t_list	*list_start;
+	list_start = ft_lstnew(c, 1);
+
+	printf("The content of the 1st box is %c", list_start->content);
+
+	return (0);
 }*/
--- libft.h
+++ libft.h
@@ -3,10 +3,9 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:03:10 by vesingh           #+#    #+#             */
-/*   Updated: 2019/05/30 12:27:05 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/05/30 12:45:39 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +14,13 @@
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
+
+typedef	struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
 
 void	ft_bzero(void *s, size_t n);
 int		ft_isdigit(int c);
@@ -74,14 +80,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-/*t_list	*ft_lstnew(void const *content, size_t conect_size);
+t_list	*ft_lstnew(void const *content, size_t conect_size);
 
 
-typdef struct	s_list;
-{
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
-}					t_list
-*/
 #endif
