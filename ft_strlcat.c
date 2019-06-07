5b515217febd52a28a8a4d6bd4f2a3e90032084e
Verona SINGH <vesingh@c4r22s4.wethinkcode.co.za>
2019-06-07 13:50:27 +0200

    fixed putnbr

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: vesingh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:31:12 by vesingh           #+#    #+#             */
-/*   Updated: 2019/05/30 11:58:55 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/06/07 12:42:59 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	srclen = ft_strlen(src);
 	dstlen = ft_strlen(dst);
-	if (dstlen > dstsize)
+	if (dstlen >= dstsize)
 		return (dstsize + srclen);
-	if (srclen < (dstsize - dstlen))
-		ft_memcpy((dst + dstlen), src, srclen + 1);
-	else
-	{
-		ft_memcpy((dst + dstlen), src, dstsize - 1);
-		dst[dstsize - 1] = '\0';
-	}
+	dst = dst + dstlen;
+	while (*src && dstsize-- > dstlen + 1)
+		*dst++ = *src++;
+	*dst = '\0';
 	return (srclen + dstlen);
 }
